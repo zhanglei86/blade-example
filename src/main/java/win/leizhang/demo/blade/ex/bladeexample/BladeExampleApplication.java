0377b8efc2caeb4b0d13d674aa33599e3ae3477e
zhanglei <zhanglei863@crnet.com.hk>
2018-07-26 11:34:39 +0800

    #1.init

---
+++ src/main/java/win/leizhang/demo/blade/ex/bladeexample/BladeExampleApplication.java
@@ -0,0 +1,12 @@
+package win.leizhang.demo.blade.ex.bladeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BladeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BladeExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/win/leizhang/demo/blade/ex/bladeexample/BladeExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package win.leizhang.demo.blade.ex.bladeexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BladeExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
